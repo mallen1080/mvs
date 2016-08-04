ea8433b68657aa2d65ed36d95ba088d54a636936
Matt Allen <matt.allen1080@gmail.com>
2016-08-04 16:20:03 -0400

    improve styling

--- frontend/playlist.jsx
+++ frontend/playlist.jsx
@@ -16,14 +16,23 @@ var Playlist = React.createClass({
     this.setState({ playlist: playlist });
   },
 
+  toggleView: function () {
+    var display = this.state.display ? 0 : 6;
+    this.setState({ display: display });
+  },
+
+  displayMore: function () {
+    this.setState({ display: this.state.display + 6 });
+  },
+
   playlistItem: function (video, i) {
     var imgUrl = video.snippet.thumbnails.medium.url;
     var artist = video.snippet.title.split(" - ")[0];
     var title = video.snippet.title.split(" - ")[1];
-    var vidId = video.snippet.resourceId.videoId;
-    
+    var vidId = video.snippet.resourceId.videoId + "?autoplay=1";
+
     return (
-      <div key={i} className="tile" onClick={this.props.changeVideo.bind(null, vidId)}>
+      <li key={i} className="tile" onClick={this.props.changeVideo.bind(null, vidId)}>
         <div className="video-img-cont">
           <img src={imgUrl} />
         </div>
@@ -31,17 +40,23 @@ var Playlist = React.createClass({
           <p className="video-artist">{artist}</p>
           <p className="video-title">{title}</p>
         </div>
-      </div>
+      </li>
     );
   },
 
   render: function () {
     var list = this.state.playlist.slice(0,this.state.display).map(this.playlistItem);
+    var klass = this.state.display ? "playlist active" : "playlist";
+    var more = this.state.display && this.state.display < 24 ?
+      <button onClick={this.displayMore}>More</button> : "";
 
     return (
-      <div className="playlist">
-        <h3>{this.props.title}</h3>
-        {list}
+      <div className={klass}>
+        <h3 onClick={this.toggleView}>{this.props.title}</h3>
+        <ul>
+          {list}
+        </ul>
+        {more}
       </div>
     );
   }
