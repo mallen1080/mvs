15c40129459254ae0a3ef336a53d3ab0e8a2a2ab
Matt Allen <matt.allen1080@gmail.com>
2016-08-05 17:22:52 -0400

    add transition to h3 underline

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Playlist = require('./playlist');
-// var apiUtil = require('./apiUtil');
 
 var Main = React.createClass({
 
