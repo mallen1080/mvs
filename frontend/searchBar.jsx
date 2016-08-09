aa2ae8619fbd2d515602e857de6735ae20fea7c5
Matt Allen <matt.allen1080@gmail.com>
2016-08-09 18:51:48 -0400

    add date to result list

--- frontend/searchBar.jsx
+++ frontend/searchBar.jsx
@@ -26,13 +26,26 @@ var SearchBar = React.createClass({
     this.setState({ results: results.items });
   },
 
+  dateAgo: function (dateString) {
+    var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    var date = new Date(dateString);
+    var today = new Date();
+
+    daysAgo = Math.floor((today - date) / _MS_PER_DAY);
+    if (daysAgo < 30) { return daysAgo + " days ago"; }
+    if (daysAgo < 365) { return Math.floor(daysAgo / 30) + " months ago"; }
+    return Math.floor(daysAgo / 360) + " years ago";
+  },
+
   searchResultItems: function () {
     var that = this;
     return this.state.results.map(function (video, i) {
       var artist = parseTitleString(video.snippet.title).artist;
       var title = parseTitleString(video.snippet.title).title;
+      var date = video.snippet.publishedAt;
+      var dateAgo = that.dateAgo(date);
       var videoId = video.id.videoId;
-      
+
       return (
         <li key={i} onClick={that.props.changeVideo.bind(null, videoId)}>
           <div className="search-img-container">
@@ -41,6 +54,7 @@ var SearchBar = React.createClass({
           <div className="search-text">
             <p>{artist}</p>
             <p>{title}</p>
+            <p>{dateAgo}</p>
           </div>
         </li>
       );
