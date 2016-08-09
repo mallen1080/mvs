f580a07e5be48565cc0b4f86cc9cfff7bad8a5a0
Matt Allen <matt.allen1080@gmail.com>
2016-08-09 16:48:30 -0400

    refactor playlists

---
+++ frontend/playlistInfo.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    display: true,
+    playlistId: "PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI",
+    title: "Popular Videos"
+  },
+
+  {
+    display: true,
+    playlistId: "PLFgquLnL59akA2PflFpeQG9L01VFg90wS",
+    title: "Newest Videos"
+  },
+
+  {
+    display: false,
+    playlistId: "PLFgquLnL59akXPIHrEZci0oouw4dArE0D",
+    title: "Electronic"
+  },
+
+  {
+    display: false,
+    playlistId: "PLFgquLnL59altZg1f_Kr1kGUYE6j-NE0M",
+    title: "Pop"
+  },
+
+  {
+    display: false,
+    playlistId: "PLH6pfBXQXHEBElcVFl-gGewA2OaATF4xL",
+    title: "Hip-Hop"
+  },
+
+  {
+    display: false,
+    playlistId: "PLhd1HyMTk3f5S98HGlByL2eH1T3n6J-bR",
+    title: "Rock"
+  }
+];
