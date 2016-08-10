8c89814541380b8819df6d32d67f3f7523d6e77b
Matt Allen <matt.allen1080@gmail.com>
2016-08-10 16:32:05 -0400

    change style of playlist to horizontal scroll

--- frontend/playlist.jsx
+++ frontend/playlist.jsx
@@ -5,12 +5,26 @@ var parseTitleString = require('./sharedFuncs').parseTitleString;
 var Playlist = React.createClass({
 
   getInitialState: function () {
-    var display = this.props.display ? 6 : 0;
+    var display = this.props.display ? 8 : 0;
     return { playlist: [], display: display };
   },
 
   componentDidMount: function () {
     apiUtil.getPlaylist(this.props.playlistId, this.receivePlaylist);
+    $(window).resize(this.handleResize);
+  },
+
+  handleResize: function () {
+    var width = $(window).width();
+
+    if (width > 768 && this.state.display !== 8) {
+      this.setState({ display: 8 });
+    } else if (width <= 768 && width > 600 && this.state.display !== 6) {
+      this.setState({ display: 6 });
+    } else if (width <= 600 && this.state.display !== 4){
+      this.setState({ display: 4 });
+    }
+
   },
 
   receivePlaylist: function (playlist) {
@@ -18,12 +32,26 @@ var Playlist = React.createClass({
   },
 
   toggleView: function () {
-    var display = this.state.display ? 0 : 6;
+    var display = this.state.display ? 0 : 8;
     this.setState({ display: display });
   },
 
   displayMore: function () {
     this.setState({ display: this.state.display + 6 });
+  },
+
+  scroll: function (direction, e) {
+    var container = $(e.target).prev();
+
+
+    if (direction === "right") {
+      var next = Math.floor((container.scrollLeft() + container.width()) / container.width()) * container.width();
+      container.animate({scrollLeft: next}, 400);
+    } else {
+      container = container.prev();
+      var prev = Math.ceil((container.scrollLeft() - container.width()) / container.width()) * container.width();
+      container.animate({scrollLeft: prev}, 400);
+    }
   },
 
   playlistItem: function (video, i) {
@@ -45,8 +73,24 @@ var Playlist = React.createClass({
     );
   },
 
+  playlistGroup: function () {
+    var playlist = this.state.playlist;
+    var playlistGroups = [];
+    if (this.state.display) {
+      for (var i = 0; i < 24; i+= this.state.display) {
+        playlistGroups.push(playlist.slice(i, i + this.state.display));
+      }
+    }
+    return playlistGroups.map(function (group, i) {
+      return <ul key={i}>{group.map(this.playlistItem)}</ul>;
+    }.bind(this));
+  },
+
   render: function () {
-    var list = this.state.playlist.slice(0,this.state.display).map(this.playlistItem);
+    var list1 = this.state.playlist.slice(0,this.state.display).map(this.playlistItem);
+    var list2 = this.state.playlist.slice(8,16).map(this.playlistItem);
+    var list3 = this.state.playlist.slice(16,24).map(this.playlistItem);
+
     var klass = this.state.display ? "playlist active" : "playlist";
     var more = this.state.display && this.state.display < 24 ?
       <button onClick={this.displayMore}>More</button> : "";
@@ -54,12 +98,13 @@ var Playlist = React.createClass({
     return (
       <div className={klass} id={this.props.title.replace(" ", "-")}>
         <h3 onClick={this.toggleView}>{this.props.title}</h3>
-        <div className="playlist-container">
-          <ul>
-            {list}
-          </ul>
+        <div className="playlist-container group">
+
+          {this.playlistGroup()}
+
         </div>
-        {more}
+        <button className="next" onClick={this.scroll.bind(null, "right")}>Next</button>
+        <button onClick={this.scroll.bind(null, "left")}>Prev</button>
       </div>
     );
   }
